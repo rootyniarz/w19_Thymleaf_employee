d50d76a0862879579027a5203f75f2fc02d928fe
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-08-21 18:44:34 +0200

    1. second kind of configuration SPRING WEB MVC - use MyWebApplicationInitializer Class (hybrid one)

---
+++ src/main/java/pl/zajavka/springwebxmlexample/MyWebApplicationInitializer.java
@@ -0,0 +1,19 @@
+package pl.zajavka.springwebxmlexample;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext container) {
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("/WEB-INF/spring-config.xml");
+        ServletRegistration.Dynamic dispatcher =
+                container.addServlet("dispatcher", new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
