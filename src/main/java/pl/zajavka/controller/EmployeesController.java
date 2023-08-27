c76ea0dfd7ce74c7e271d9982f61678a4e237078
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-08-27 17:59:26 +0200

    5. added concurency - 13 Aplikacje Web a wielowątkowość - cz.1 (done)

--- src/main/java/pl/zajavka/controller/EmployeesController.java
+++ src/main/java/pl/zajavka/controller/EmployeesController.java
@@ -1,5 +1,6 @@
 package pl.zajavka.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Random;
 
+@Slf4j
 @Controller
 @RequestMapping("/employees")
 public class EmployeesController {
-    
+
     @GetMapping
-    public String employees(final Model model) { 
-        model.addAttribute("randomValue", randomInt(1, 10));
+    public String employees(final Model model) {
+        int randomValue = randomInt(1, 10);
+        log.info("Random: [{}]", randomValue);
+        model.addAttribute("randomValue", randomValue);
         return "employees"; // wyswietl plik employees.jsp
     }
 
     private int randomInt(final int min, final int max) {
         return new Random().nextInt(max - min) + min;
     }
+
 
 }
