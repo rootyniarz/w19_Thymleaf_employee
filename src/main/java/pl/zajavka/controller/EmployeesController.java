6bb5d3af80493d446ca5da25f77f42721cb9b02b
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-08-27 22:34:46 +0200

    6. added concurency and code to test (JMeter) thread safe - 13 Aplikacje Web a wielowątkowość - cz.2 (in progress)

--- src/main/java/pl/zajavka/controller/EmployeesController.java
+++ src/main/java/pl/zajavka/controller/EmployeesController.java
@@ -7,17 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @Controller
 @RequestMapping("/employees")
 public class EmployeesController {
 
+    private int count;
+    AtomicInteger atomicCount = new AtomicInteger(0);
+
     @GetMapping
     public String employees(final Model model) {
-        int randomValue = randomInt(1, 10);
-        log.info("Random: [{}]", randomValue);
-        model.addAttribute("randomValue", randomValue);
+        int countPlus = ++count;
+        int atomicCountPlus = atomicCount.incrementAndGet();
+        log.info("count: [{}]", countPlus);
+        log.info("atomicCount: [{}]", atomicCountPlus);
+        model.addAttribute("count", countPlus);
+        model.addAttribute("atomicCount", atomicCountPlus);
         return "employees"; // wyswietl plik employees.jsp
     }
 
--- src/main/webapp/WEB-INF/view/employees.jsp
+++ src/main/webapp/WEB-INF/view/employees.jsp
@@ -1,21 +1,11 @@
-<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<!DOCTYPE html>
 <html>
 <head>
   <title>Employees</title>
 </head>
 <body>
-  <h1>Employees</h1>
-  <c:choose>
-  <c:when test="${randomValue % 2 == 0}">
-  <p>Even</p>
-  </c:when>
-  <c:otherwise>
-  <p>Odd</p>
-  </c:otherwise>
-  </c:choose>
-  <br>
-  <h2>${randomValue}</h2>
+<h1>Employees</h1>
+<p>Count: ${count}, AtomicCount: ${atomicCount}</p>
 </body>
 </html>
