1993a0795c6d694bcf52424f7e12002a28e52b92
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-09-03 22:42:20 +0200

    1. start ApplicationConfiguration.java and ServletDispatcherInitializer.java

--- src/main/java/pl/zajavka/configuration/ApplicationConfiguration.java
+++ src/main/java/pl/zajavka/configuration/ApplicationConfiguration.java
@@ -1,20 +1,4 @@
 package pl.zajavka.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-@Configuration
-@ComponentScan("pl.zajavka")
-@EnableWebMvc
 public class ApplicationConfiguration {
-    @Bean
-    public InternalResourceViewResolver internalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/view/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
 }
---
+++ src/main/java/pl/zajavka/configuration/ServletDispatcherInitializer.java
@@ -0,0 +1,31 @@
+package pl.zajavka.configuration;
+
+import org.springframework.lang.NonNull;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import jakarta.servlet.Filter;
+public class ServletDispatcherInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ApplicationConfiguration.class};
+    }
+    @NonNull
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{new HiddenHttpMethodFilter(), characterEncodingFilter};
+    }
+}
