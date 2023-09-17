33fdfc0a508aa4680463cc3212492677b65433e5
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-09-17 22:10:48 +0200

    6. 18 Spring Web MVC - Thymeleaf - kontrolery - POST oraz GET - cz.1 DONE

--- src/main/java/pl/zajavka/controller/EmployeesController.java
+++ src/main/java/pl/zajavka/controller/EmployeesController.java
@@ -2,14 +2,32 @@ package pl.zajavka.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.zajavka.infrastructure.database.entity.EmployeeEntity;
 import pl.zajavka.infrastructure.database.repository.EmployeeRepository;
+
+import java.math.BigDecimal;
 
 @Controller
 @RequestMapping("/employees")
 @AllArgsConstructor
 public class EmployeesController {
     private EmployeeRepository employeeRepository;
-    // implementacja kontrolera
+    @PostMapping("/add")
+    public String addEmployee(
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "surname") String surname,
+            @RequestParam(value = "salary") String salary
+    ) {
+        EmployeeEntity newEmployee = EmployeeEntity.builder()
+  .name(name)
+                .surname(surname)
+                .salary(new BigDecimal(salary))
+                .build();
+        employeeRepository.save(newEmployee);
+        return "redirect:/employees";
+    }
 }
 
